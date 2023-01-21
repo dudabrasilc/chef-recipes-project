8e1254f254966bb66dccd187977d1a8d4156653f
Maria Coitinho <brasildu@gmail.com>
2023-01-20 20:10:10 -0600

    Create show, index and update actions for comments controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+  rescue_from ActiveRecord::RecordNotFound, with:
+  :render_not_found
+
+  # before_action :authorize
+
+private
+
+  # def authorize
+  #   return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  # end
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found(error)
+    byebug
+    render json: { errors: error.message }, status: :not_found
+  end
+
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,34 @@
 class CommentsController < ApplicationController
+
+# GET /comments
+def index
+  comments = Comment.all
+  render json: comments, status: :ok
+end
+
+# GET /comments/:id
+def show
+  byebug
+  comment = Comment.find(params[:id])
+  render json: comment, status: :ok
+end
+
+# PATCH /comments/:id
+def update
+  byebug
+  comment = Comment.find(params[:id])
+  comment.update(description: params[:description])
+  # render json: comment, status: :ok
+end
+
+
+
+private
+
+def comment_params
+  params.permit(:description, :recipe_id, :user_id)
+end
+
+
+
 end

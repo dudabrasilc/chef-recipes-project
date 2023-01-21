7cbdc723908381dc41c76eba15f3d26dbe9314e7
Maria Coitinho <brasildu@gmail.com>
2023-01-20 21:19:54 -0600

    Configure comment serializer to have a recipe and user on the data to be sent to the client

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   end
 
 
-  private
+private
 
   def user_params
       params.permit(:username, :password)
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :description, :recipe_id, :user_id
+
+  belongs_to :user
+  belongs_to :recipe
 end

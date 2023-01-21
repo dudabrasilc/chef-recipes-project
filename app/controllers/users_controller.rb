2b7b62adb8b384cc867b91c4d63be7726c33adb4
Maria Coitinho <brasildu@gmail.com>
2023-01-20 21:11:47 -0600

    Add skip before action macro to sessions controller, so the client dont need to have a hash session in order to login. Build create action to handle login and destroy action for logging out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  # POST '/signup'
+  # OK
+  def create
+      # byebug
+      user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: user, status: :created
+  end
+
+  # GET '/me'
+  # ???
+  def show
+      render json: @current_user
+  end
+
+
+  private
+
+  def user_params
+      params.permit(:username, :password)
+  end
+
 end

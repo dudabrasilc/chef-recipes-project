c4b371a1465565fc9bc59a2c11d3d93a849e6048
Maria Coitinho <brasildu@gmail.com>
2023-01-22 19:43:38 -0600

    Add routes on the backend to handle client requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   
-    # resources :sessions, only: [:create]
-    resources :comments
-    resources :recipes, only: [:index, :show, :update, :create, :destroy]
-  
-    post "/signup", to: "users#create"
-    get "/me", to: "users#show"
-  
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
+  # resources :sessions, only: [:create]
+  resources :comments
+  resources :recipes, only: [:index, :show, :update, :create, :destroy]
 
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
